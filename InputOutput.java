a68c0a0d13394cfded65eda930bbbf83affd3e62
aswatun <namakuaswa@gmail.com>
2020-04-12 22:54:17 +0800

    Buat File Input Output

---
+++ InputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputOutput{
+ public static void main(String [] args){
+  Scanner Scanner = new Scanner(System.in);
+  String nama;
+  System.out.print("Masukkan nama anda");
+  nama = Scanner.nextLine();
+  System.out.println("Hai, "+nama");
+
+ }
+}
